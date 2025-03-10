822e5d5b97b6b2b1eacd5df811ceae347cf672e3
Oz-Nazhad <l.weinberger@tutamail.com>
2025-03-10 13:16:55 +0100

    normal gridLayout

---
+++ Neurodi/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_appNeurodi_FILE /home/oz/Projekte/Neurodi/Neurodi/build/Desktop-Debug/appNeurodi)
+set(__QT_DEPLOY_TARGET_appNeurodi_TYPE EXECUTABLE)
---
+++ Neurodi/build/Desktop-Debug/.qt/appNeurodi_qml.cmake
@@ -0,0 +1,9 @@
+
+set(target "appNeurodi")
+set(working_dir "/home/oz/Projekte/Neurodi/Neurodi")
+set(src_and_dest_list
+    "/home/oz/Projekte/Neurodi/Neurodi/Main.qml"
+    "/home/oz/Projekte/Neurodi/Neurodi/build/Desktop-Debug/Neurodi/Main.qml"
+
+)
+set(timestamp_file "/home/oz/Projekte/Neurodi/Neurodi/build/Desktop-Debug/.qt/appNeurodi_qml.txt")
